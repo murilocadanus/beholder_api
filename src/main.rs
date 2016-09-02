3e4b067e554aa9f1fb3a4f580fac57859ffbe65b
Murilo Cadanus da Costa <murilocadanus@gmail.com>
2016-09-02 16:02:53 -0300

    [FIX] Moved authentication to util package

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ mod util;
 
 // nickel
 use nickel::{Nickel, JsonBody, HttpRouter, MediaType, Request, Response, MiddlewareResult};
-use nickel::status::StatusCode::{self, Forbidden};
+use nickel::status::StatusCode;
 
 // hyper
 use hyper::header;
@@ -365,7 +365,7 @@ fn main() {
 	});
 
 	// Use a filter to every route
-	server.utilize(authenticator);
+	server.utilize(util::authenticator);
 	info!("Using filter with server.");
 
 	// Use the defined routes
@@ -374,44 +374,4 @@ fn main() {
 
 	// Listen at url defined
 	server.listen(server_url.as_str());
-}
-
-fn authenticator<'mw>(request: &mut Request, response: Response<'mw>) -> MiddlewareResult<'mw> {
-	// Check if we are getting an OPTIONS request
-	if request.origin.method.to_string() == "OPTIONS".to_string() {
-		// The middleware should not be used for OPTIONS, so continue
-		response.next_middleware()
-
-	} else {
-		// We do not want to apply the middleware to the login route
-		if request.origin.uri.to_string() == "/login".to_string() {
-			response.next_middleware()
-		} else {
-			// Get the full Authorization header from the incoming request headers
-			let auth_header = match request.origin.headers.get::<Authorization<Bearer>>() {
-				Some(header) => header,
-				None => panic!("No authorization header found")
-			};
-
-			// Format the header to only take the value
-			let jwt = header::HeaderFormatter(auth_header).to_string();
-
-			// We don't need the Bearer part,
-			// so get whatever is after an index of 7
-			let jwt_slice = &jwt[7..];
-
-			// Parse the token
-			let token = Token::<Header, Registered>::parse(jwt_slice).unwrap();
-
-			// Get the secret key as bytes
-			let secret = AUTH_SECRET.as_bytes();
-
-			// Verify the token
-			if token.verify(&secret, Sha256::new()) {
-				response.next_middleware()
-			} else {
-				response.error(Forbidden, "Access denied")
-			}
-		}
-	}
 }
--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,53 @@
 // nickel
 use nickel::{Request, Response, MiddlewareResult};
+use nickel::status::StatusCode::{self, Forbidden};
 
 // hyper
 use hyper::header;
 use hyper::header::{Authorization, Bearer};
 
+use crypto::sha2::Sha256;
 use jwt::{Header, Registered, Token};
+
+
+use AUTH_SECRET;
+
+pub fn authenticator<'mw>(request: &mut Request, response: Response<'mw>) -> MiddlewareResult<'mw> {
+	// Check if we are getting an OPTIONS request
+	if request.origin.method.to_string() == "OPTIONS".to_string() {
+		// The middleware should not be used for OPTIONS, so continue
+		response.next_middleware()
+
+	} else {
+		// We do not want to apply the middleware to the login route
+		if request.origin.uri.to_string() == "/login".to_string() {
+			response.next_middleware()
+		} else {
+			// Get the full Authorization header from the incoming request headers
+			let auth_header = match request.origin.headers.get::<Authorization<Bearer>>() {
+				Some(header) => header,
+				None => panic!("No authorization header found")
+			};
+
+			// Format the header to only take the value
+			let jwt = header::HeaderFormatter(auth_header).to_string();
+
+			// We don't need the Bearer part,
+			// so get whatever is after an index of 7
+			let jwt_slice = &jwt[7..];
+
+			// Parse the token
+			let token = Token::<Header, Registered>::parse(jwt_slice).unwrap();
+
+			// Get the secret key as bytes
+			let secret = AUTH_SECRET.as_bytes();
+
+			// Verify the token
+			if token.verify(&secret, Sha256::new()) {
+				response.next_middleware()
+			} else {
+				response.error(Forbidden, "Access denied")
+			}
+		}
+	}
+}
